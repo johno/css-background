cce97b65819791aa125019da6887ff4e94bc3ab1
John Otander <johnotander@gmail.com>
2015-01-29 16:40:19 -0700

    Add some failing tests

--- test/test.js
+++ test/test.js
@@ -3,7 +3,31 @@ var cssBackground = require('..');
 
 describe('css-background', function() {
 
-  it('should do something awesome', function() {
-    assert.equal(cssBackground(), true);
+  it('should parse single colors', function() {
+    assert.deepEqual(cssBackground('green'), { color: 'green' });
+  });
+
+  it('should parse a value with color, image, repeat and position specified', function() {
+    assert.deepEqual(
+      cssBackground('#ffffff url("img_tree.png") no-repeat right top'),
+      {
+        color: '#ffffff',
+        image: 'url("img_tree.png")',
+        repeat: 'no-repeat',
+        position: 'right top'
+      }
+    );
+  });
+
+  it('should parse a value with an image, repeat, and attachment', function() {
+    assert.deepEqual(
+      cssBackground('url("topbanner.png") #00D repeat-y fixed'),
+      {
+        image: 'url("topbanner.png")',
+        color: '#00D',
+        repeat: 'repeat-y',
+        attachement: 'fixed'
+      }
+    );
   });
 });
