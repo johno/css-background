4439ca0650e7595b5d6cadb03fba1de645e03e81
John Otander <johnotander@gmail.com>
2015-01-29 16:12:16 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var cssBackground = require('..');
+
+describe('css-background', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(cssBackground(), true);
+  });
+});
